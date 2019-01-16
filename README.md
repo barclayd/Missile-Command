e7ba4240af5d10ca3270eb5a53565d1727ab32f4
Daniel Barclay <barclaysd@me.com>
2019-01-16 10:57:58 +0000

    Initial commit 🚀

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Missile-Command
-Arcade inspired Missile Command game built using Python
+# Missile Command 🚀
+
+Atari arcade inspired Missile Command game built using Python with (SPGL game engine)[https://github.com/wynand1004/SPGL]
---
+++ missile_command.py
@@ -0,0 +1,21 @@
+# Import SPGL
+import spgl
+
+# Classes
+
+# Functions
+
+# Initial Game setup
+game = spgl.Game(800, 600, "black", "Missile Command")
+
+# Sprites
+
+# Labels
+
+# Buttons
+
+# Keyboard Bindings
+
+while True:
+    # Call the game tick method
+    game.tick()
